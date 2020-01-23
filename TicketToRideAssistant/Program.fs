d18d7c274ac4761c32572f97af898345642ffef5
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2020-01-22 21:19:11 -0800

    added more cities and routes

--- TicketToRideAssistant/Program.fs
+++ TicketToRideAssistant/Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Runtime.CompilerServices
 
 type color_t =
     | Blue
@@ -38,10 +39,16 @@ let main argv =
     let duluth = { name = "Duluth" }
     let omaha = { name = "Omaha" }
     let denver = { name = "Denver" }
-    let saultmaria = { name = "Sault St. Marie" }
+    let saultmarie = { name = "Sault St. Marie" }
     let toronto = { name = "Toronto" }
     let montreal = { name = "Montreal" }
     let chicago = { name = "Chicago" }
+    let kansas = { name = "Kansas City" }
+    let vegas = { name = "Las Vegas" }
+    let losangeles = { name = "Los Angeles" }
+    let phoenix = { name = "Phoenix" }
+    let elpaso = { name = "El Paso" }
+    let santafe = { name = "Santa Fe" }
     
     let routes = seq {
         { from_city = vancouver; to_city = seattle; trains = 1; color = None; }
@@ -65,15 +72,29 @@ let main argv =
         { from_city = saltlake; to_city = denver; trains = 3; color = Some Red; }
         { from_city = saltlake; to_city = denver; trains = 3; color = Some Yellow; }
         { from_city = denver; to_city = omaha; trains = 4; color = Some Pink; }
-        { from_city = winnipeg; to_city = saultmaria; trains = 6; color = None; }
-        { from_city = duluth; to_city = saultmaria; trains = 3; color = None; }
+        { from_city = winnipeg; to_city = saultmarie; trains = 6; color = None; }
+        { from_city = duluth; to_city = saultmarie; trains = 3; color = None; }
         { from_city = duluth; to_city = toronto; trains = 6; color = Some Pink; }
-        { from_city = saultmaria; to_city = toronto; trains = 2; color = None; }
+        { from_city = saultmarie; to_city = toronto; trains = 2; color = None; }
         { from_city = toronto; to_city = montreal; trains = 3; color = None; }
-        { from_city = saultmaria; to_city = montreal; trains = 5; color = Some Black; }
+        { from_city = saultmarie; to_city = montreal; trains = 5; color = Some Black; }
         { from_city = duluth; to_city = omaha; trains = 2; color = None; }
         { from_city = duluth; to_city = chicago; trains = 3; color = Some Red; }
         { from_city = omaha; to_city = chicago; trains = 4; color = Some Blue; }
+        { from_city = omaha; to_city = kansas; trains = 1; color = None; }
+        { from_city = denver; to_city = kansas; trains = 4; color = Some Black; }
+        { from_city = denver; to_city = kansas; trains = 4; color = Some Orange; }
+        { from_city = saltlake; to_city = vegas; trains = 3; color = Some Orange; }
+        { from_city = vegas; to_city = losangeles; trains = 2; color = None; }
+        { from_city = sanfran; to_city = losangeles; trains = 3; color = Some Pink; }
+        { from_city = sanfran; to_city = losangeles; trains = 3; color = Some Yellow; }
+        { from_city = losangeles; to_city = phoenix; trains = 3; color = None; }
+        { from_city = losangeles; to_city = elpaso; trains = 6; color = Some Black; }
+        { from_city = phoenix; to_city = elpaso; trains = 3; color = None; }
+        { from_city = phoenix; to_city = denver; trains = 5; color = Some White; }
+        { from_city = phoenix; to_city = santafe; trains = 3; color = None; }
+        { from_city = elpaso; to_city = santafe; trains = 2; color = None; }
+        { from_city = santafe; to_city = denver; trains = 2; color = None; }
     }
     
     let createOtherDirection (r:route_t) =
@@ -81,6 +102,25 @@ let main argv =
             r
             { r with from_city = r.to_city; to_city = r.from_city; }
         }
+    
+    let cities = Set.ofList [
+        vancouver
+        seattle
+        portland
+        calgary
+        helena
+        winnipeg
+        saltlake
+        sanfran
+        duluth
+        omaha
+        denver
+        saultmarie
+        toronto
+        montreal
+        chicago
+        kansas;
+    ]
     
     let map =
         routes
@@ -90,7 +130,49 @@ let main argv =
         |> Map.ofSeq
     
     let connections = map.[calgary]
+
+    let filterRoutes routes =
+        routes
+        |> Seq.groupBy (fun r -> r.to_city)
+        |> Seq.map (fun (_,r) -> Seq.item(0) r)
+        |> Seq.toList
+        
+    //TODO - Dijkstra's for shortest path
+    //TODO - minimum spanning tree to find shortest connection that spans multiple nodes, ie: shortest path connecting x cities
+        
+    let notContains city state =
+        state |> Set.contains city |> not
+         
+         
+         
+         
+         
+    let rec shortestPath current last state distances =            
+        if Set.contains last state then
+            state
+        else
+            let unvisitedAdjacents =
+                map.[current]
+                |> Seq.filter (fun r -> (notContains r.to_city state))
+                |> Seq.fold (fun (m:Map<city_t,int>) r ->                        
+                        let current = if m.ContainsKey r.to_city then m.[r.to_city] else 0 
+                        m.Add (r.to_city, (current + r.trains))
+                    ) distances
+            let (closestCity,_) =
+                unvisitedAdjacents
+                |> Map.toSeq
+                |> Seq.minBy (fun (_,trains) -> trains)
+            let newState = state.Add closestCity
+            shortestPath closestCity last newState unvisitedAdjacents
+        
+    let map =
+        Map.empty
+        |> Map.add sanfran 0
+        
+    let path = shortestPath sanfran kansas Set.empty map
     
     printfn "Hello World from F#!"
     
     0 // return an integer exit code
+
+
